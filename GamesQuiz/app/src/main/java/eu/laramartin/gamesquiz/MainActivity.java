36dcc48f6ca2a77cd16358b94672bcc75f32c1aa
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-19 20:20:52 +0200

    button visible only when user has chosen

--- GamesQuiz/app/src/main/java/eu/laramartin/gamesquiz/MainActivity.java
+++ GamesQuiz/app/src/main/java/eu/laramartin/gamesquiz/MainActivity.java
@@ -4,7 +4,10 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
     TextView optionOneTextView;
     TextView optionTwoTextView;
     TextView optionThreeTextView;
+    Button nextButton;
     Quote optionOneQuote;
     Quote optionTwoQuote;
     Quote optionThreeQuote;
     Quote actualQuote;
+    boolean isTurnFinished = false;
+    boolean isGameFinished = false;
 
     int quotesDisplayed = 0;
 
@@ -38,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         optionOneTextView = (TextView) findViewById(R.id.optionOne);
         optionTwoTextView = (TextView) findViewById(R.id.optionTwo);
         optionThreeTextView = (TextView) findViewById(R.id.optionThree);
-
+        nextButton = (Button) findViewById(R.id.buttonNext);
 
         GameQuotes.initQuotes(listOfQuotes);
         shuffleList(listOfQuotes);
@@ -55,18 +61,21 @@ public class MainActivity extends AppCompatActivity {
     final View.OnClickListener choiceOneOnClickListener = new View.OnClickListener() {
         public void onClick(final View v){
             isOptionChosenCorrect(0);
+            isTurnFinished = true;
         }
     };
 
     final View.OnClickListener choiceTwoOnClickListener = new View.OnClickListener() {
         public void onClick(final View v){
             isOptionChosenCorrect(1);
+            isTurnFinished = true;
         }
     };
 
     final View.OnClickListener choiceThreeOnClickListener = new View.OnClickListener() {
         public void onClick(final View v){
             isOptionChosenCorrect(2);
+            isTurnFinished = true;
         }
     };
 
@@ -145,13 +154,34 @@ public class MainActivity extends AppCompatActivity {
 
     private void markOptionChosenAsCorrect(TextView chosen){
         chosen.setBackgroundColor(Color.parseColor("#49C684"));
-
+        makeNextButtonVisible();
+        checkIfGameHasFinished();
     }
 
     private void markOptionChosenAsWrong(TextView chosen){
-       chosen.setBackgroundColor(Color.parseColor("#F55E7A"));
-
+        chosen.setBackgroundColor(Color.parseColor("#F55E7A"));
+        makeNextButtonVisible();
+        checkIfGameHasFinished();
     }
 
+    private void checkIfGameHasFinished(){
+        if (! isGameFinished){
+            Toast.makeText(MainActivity.this, "game ended", Toast.LENGTH_SHORT).show();
+        } else {
+            beginNextTurn();
+        }
+    }
+
+    private void beginNextTurn(){
+        makeNextButtonInvisible();
+    }
+
+    private void makeNextButtonVisible(){
+        nextButton.setVisibility(View.VISIBLE);
+    }
+
+    private void makeNextButtonInvisible(){
+        nextButton.setVisibility(View.INVISIBLE);
+    }
 
 }
